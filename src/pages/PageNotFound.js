53f5f4d52548a443a0954a5e8541c4e4259fdebe
Your GitHub Username <pooja@cloudlion.org>
2024-03-23 15:45:44 -0400

    Updated error

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,12 +1,12 @@
 import page from "../assets/yes.png"
-import { Link } from "react-router-dom"
+
 export const PageNotFound = () => {
     return (
      <main>
        <section className="flex flex-col justify-center px-2">
         <div className="flex flex-col items-center my-4">
           <p className="text-7xl text-gray-700  font-bold my-10 dark:text-white"> Oops! </p>
-          <img className="rounded max-w-lg" src={page}/>
+          <img className="rounded max-w-lg" src={page} alt="Page not found"/>
         </div>
         <div>
           
